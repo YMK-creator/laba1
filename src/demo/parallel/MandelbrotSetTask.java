3863b3093561826d869f4b14dedce9a00f93839f
YMK-creator <morozovarik72@gmail.com>
2025-09-04 14:02:37 +0300

    Изменил цветовую гамму

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
-            Color.RED,
-            Color.WHITE,
-            Color.RED,
-            Color.rgb(100, 0, 0),
-            Color.RED,
-            Color.rgb(50, 0, 0)
+                Color.rgb(20, 0, 40),      // тёмный фиолетовый (почти чёрный с фиолетовым оттенком)
+                Color.PURPLE,              // основной фиолетовый
+                Color.MEDIUMPURPLE,        // мягкий средний фиолетовый
+                Color.VIOLET,              // светло-фиолетовый
+                Color.ORCHID,              // сиреневый
+                Color.PLUM,                // нежно-фиолетовый
+                Color.rgb(240, 230, 250)   // очень светлый (lavender)
         };
         
         /**
